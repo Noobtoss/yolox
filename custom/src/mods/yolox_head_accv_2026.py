efdf013d060e18ecb841111016967d290df5436f
Noobtoss <thomasschmittba17196@gmail.com>
2026-09-17 15:53:51 +0200

    removed unused cls_feat_loss mask and weight funcs

--- custom/src/mods/yolox_head_accv_2026.py
+++ custom/src/mods/yolox_head_accv_2026.py
@@ -294,11 +294,7 @@ class YOLOXHead(_YOLOXHead):
         reg_weight = 5.0
         # >>> MOD
         loss_cls_feats = (
-            self.cls_feat_loss(
-                cls_feats=cls_feats,
-                target_scores=cls_targets,
-                pred_scores=cls_preds.view(-1, self.num_classes)[fg_masks].detach(),
-            )
+            self.cls_feat_loss(cls_feats=cls_feats, target_scores=cls_targets)
         )
         loss = reg_weight * loss_iou + loss_obj + loss_cls + self.cls_feat * loss_cls_feats + loss_l1
 
